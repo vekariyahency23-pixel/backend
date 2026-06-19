3d82b79b671e9d0792e2aa28dde6199eb79f1fa3
hency <vekariyahenct23@gmil.com>
2026-06-19 17:26:39 +0530

    Initial commit

--- routes/inquiryRoutes.js
+++ routes/inquiryRoutes.js
@@ -1,32 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
-
 const Inquiry = require("../models/Inquiry");
 
 /* ================= SAVE INQUIRY ================= */
 
-router.post("/", async (req, res) => {
-
-  try {
-
-    const inquiry = new Inquiry(req.body);
-
-    await inquiry.save();
-
-    res.status(201).json({
-      success: true,
-      message: "Inquiry Submitted Successfully",
-    });
-
-  } catch (error) {
-
-    res.status(500).json({
-      success: false,
-      message: "Server Error",
-    });
-
-  }
 router.post("/", async (req, res) => {
   console.log("INQUIRY HIT:", req.body);
 
@@ -41,9 +18,12 @@ router.post("/", async (req, res) => {
 
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Server Error" });
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
   }
-});
 });
 
 module.exports = router;
